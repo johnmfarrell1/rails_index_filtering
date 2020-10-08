011c54787af64a5dc7d1f26b842305399867cbed
johnmfarrell1 <john.m.farrell1@gmail.com>
2020-10-08 12:47:07 +0100

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.filter(filtering_params)
+    @users = User.filter(filtering_params).page(params[:page])
   end
 
   private
