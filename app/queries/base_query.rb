356baa23a7420d73f0f8ae596cf6a935f770744f
johnmfarrell1 <john.m.farrell1@gmail.com>
2020-10-09 15:34:01 +0100

    make call abstract method in base query class (template method)

--- app/queries/base_query.rb
+++ app/queries/base_query.rb
@@ -1,11 +1,15 @@
 class BaseQuery
   attr_reader :relation
 
+  class << self
+    delegate :call, to: :new
+  end
+
   def initialize(relation = self.class.module_parent)
     @relation = relation
   end
 
-  class << self
-    delegate :call, to: :new
+  def call
+    raise NotImplementedError, 'call method must implement query logic'
   end
 end
