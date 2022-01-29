98f5cf1eeec4270aec6eaed9d055122b29a50ece
johnmfarrell1 <john.m.farrell1@gmail.com>
2022-01-29 11:31:16 +0000

    update controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.filter(filtering_params).page(params[:page])
+    @users = User.filter_by(filtering_params).page(params[:page])
   end
 
   private
